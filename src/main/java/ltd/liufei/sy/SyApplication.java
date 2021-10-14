70591a3073254b127700bc9cc72e30655bc8064e
liufei <me@liufei.ltd>
2021-10-14 10:39:09 +0800

    init

---
+++ src/main/java/ltd/liufei/sy/SyApplication.java
@@ -0,0 +1,13 @@
+package ltd.liufei.sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ltd/liufei/sy/SyApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.liufei.sy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
